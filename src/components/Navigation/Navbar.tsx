71d62f8e8f667addd8394d1fca1c06f2f9b46d0c
CalderonCodes <cgcalderondev@gmail.com>
2025-03-31 22:39:07 -0600

    fix: fix navbar icon :bug:

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="bg-[#191919] p-4 w-full shadow-2xl shadow-black-700/50">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold flex gap-2 items-center">
-          <img src={"/public/pokeball.svg"} className="h-6" alt="" />
+          <img src={"/pokeball.svg"} className="h-6" alt="" />
           <h1>Pokedex</h1>
         </div>
 
